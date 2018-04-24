fe7f6c412f619fb4f8acebcfa208fbb2e5c1bb46
hotuta <hotuta@users.noreply.github.com>
2018-04-24 15:34:08 +0900

    Add clock.rb

---
+++ config/clock.rb
@@ -0,0 +1,13 @@
+require 'clockwork'
+require './config/boot'
+require './config/environment'
+
+module Clockwork
+  handler do |job|
+    puts "Running #{job}"
+  end
+
+  every(1.hours, 'Epark::Takeout::Shop') do
+    Epark::Takeout::Shop.get_shop_and_product
+  end
+end
